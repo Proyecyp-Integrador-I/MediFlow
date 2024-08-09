6b7ca79bbf956de9ce9ed801d8a87c9ba5b60972
Juan Jara <jaracalle2041@gmail.com>
2024-08-09 11:33:58 -0500

    New-exam URL added and templates folder created

--- mediflow/urls.py
+++ mediflow/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mediflowApp import views as mediflowViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mediflowViews.home),
+    path('new-exam/', mediflowViews.new_exam)
 ]
